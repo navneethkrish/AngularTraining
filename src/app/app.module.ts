e73f3a742132131b827e1c6c49020871684317b0
navneethkrish <navaneeth@ideas2it.com>
2020-01-23 20:53:07 +0530

    feature: Implement various types of pipes in angular   - Services and dependency injection          - Fetching data using the Http client          - Handle error while using catch method         - Learned about routing and implemented 

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,33 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component'; 
+import { HttpClientModule } from '@angular/common/http';
+import { HeroService } from './hero.service';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, 
+      { dataEncapsulation: false }
+    )
   ],
-  providers: [],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
